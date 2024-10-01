746a29b4c996b88519e2b87ce0795c310d29d947
Medhavimangal43 <medhavimangal43@gmail.com>
2024-10-01 23:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//your JS code here. If required.
 let first=prompt("Enter first number")
 let firstint=parseInt(first)
 let second=prompt("Enter second number")
 let secondint=parseInt(second)
 let result=firstint*2+secondint
 
-
-alert("The final output is:"+" " + result)
+alert("The final output is: " + result)
